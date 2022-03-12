44d4614d0b42d911d4e7082018ab1e87eed0cf2c
OSSnewb <kik1232198@gmail.com>
2022-03-12 16:40:39 +0900

    first commit

--- practice.py
+++ practice.py
@@ -1,25 +1,27 @@
-d=[]
+maze=[[0 for i in range(10)]for j in range(10)]
 for i in range(10):
-    b=list(map,(int,(input().split())))
-    d.append(b)
+    maze[i]=list(map(int,input().split()))
 
 x,y=1
+
 while True:
-    if(d[x][y]==0):
-        d[x][y]=9
-    elif(d[x][y]==2):
-        d[x][y]==9
-        break #영역 표시
-    if (d[x][y+1]!=1):
-        y=y+1
-    elif(d[x+1][y]!=1):
-        x=x+1
-    if(d[x][y+1]==1 and d[x+1][y]==1):
+    if maze[x][y+1]==0:
+        maze[x][y]=9
+        y+=1
+    elif maze[x+1][y]==0:
+        maze[x][y]=9
+        x+=1
+    elif maze[x][y+1]==2:
+        maze[x][y]=9
+        maze[x][y+1]=9
+        break
+    elif maze[x+1][y]==2:
+        maze[x][y]=9
+        maze[x+1][y]=9
+        break
+    else:
+        maze[x][y]=9
         break
 
-for i in range(10):
-    for j in range(10):
-        print(d[i][j],end=" ")
-    print("")
-
-    
+for i in range(len(maze)):
+    print(' '.join(map(str,maze[i])))
